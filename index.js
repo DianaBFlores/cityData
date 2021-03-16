12127989021aabb14576f5871849fea387646125
DianaBFlores <64798817+DianaBFlores@users.noreply.github.com>
2021-03-15 21:39:58 -0400

    updated

--- index.js
+++ index.js
@@ -1,12 +1,28 @@
 
     async function cityLongAndLat(){
-        let cityName = document.getElementById("search").value; //allows the cityName to equal the value in the search bar
+        let cityName = document.getElementById("search").value; 
         let response = await fetch("https://geocode.xyz/" + cityName + "?json=1"); 
         let data = await response.json(); 
     
-        let longitude = data.longt; //fetching specifically the longitude
-        let latitude = data.latt; //fetching specifically the latitude
+        let longitude = data.longt; //gets longitude
+        let latitude = data.latt; //get lattitude
         
-        console.log(longitude +"," + latitude); //printing the long. and lat. in the console
+        console.log(longitude +"," + latitude); //
       
     }
+       cityLongAndLat();
+
+       
+  //   async function loadJson(url) { 
+  //     let response = await fetch(url); 
+    
+  //     if (response.status == 200) {
+  //       let json = await response.json(); 
+  //       return responce.json;
+  //     }
+  //     else{
+  //     throw new Error(response.status);
+  //   }
+  // }
+  //   loadJson('http://httpstat.us/500');
+       
